0fbeddeedf687d4f823822f67c3549585ec1d4bb
hwi0523 <101933886+hwi0523@users.noreply.github.com>
2022-06-29 18:15:09 +0900

    MVC

--- application/controllers/FeedController.php
+++ application/controllers/FeedController.php
@@ -1,26 +1,18 @@
 <?php
 namespace application\controllers;
 
-use PDO;
-
-class FeedController extends Controller{
-    public function index(){
+class FeedController extends Controller {
+    public function index() {
         $this->addAttribute(_JS, ["feed/index"]);
+        $this->addAttribute(_CSS, ["feed/index"]);
         $this->addAttribute(_MAIN, $this->getView("feed/index.php"));
         return "template/t1.php";
     }
 
-    public function rest(){
-        switch(getMethod()){
+    public function rest() {       
+        switch(getMethod()) {
             case _POST:
-                // if(is_array($_FILES)){
-                //     foreach($_FILES['imgs']['name'] as $key => $value){
-                //         print "key : {$key}, value : {$value} <br>";
-                //     }
-                // }
-                // print "ctnt : " . $_POST["ctnt"] . "<br>";
-                // print "location : " . $_POST["location"] . "<br>";
-                if(!is_array($_FILES) || !isset($_FILES["imgs"])){
+                if(!is_array($_FILES) || !isset($_FILES["imgs"])) {
                     return ["result" => 0];
                 }
                 $iuser = getIuser();
@@ -28,28 +20,44 @@ class FeedController extends Controller{
                     "location" => $_POST["location"],
                     "ctnt" => $_POST["ctnt"],
                     "iuser" => $iuser
-                ];
+                ];                
                 $ifeed = $this->model->insFeed($param);
-                
-                foreach($_FILES["imgs"]["name"] as $key => $originFileNm){
 
+                $paramImg = [ "ifeed" => $ifeed ];
+                foreach($_FILES["imgs"]["name"] as $key => $originFileNm) {
+                                        
                     $saveDirectory = _IMG_PATH . "/feed/" . $ifeed;
-                    if(!is_dir($saveDirectory)){
+                    if(!is_dir($saveDirectory)) {
                         mkdir($saveDirectory, 0777, true);
                     }
-                    $tempName = $_FILES["imgs"]["tmp_name"][$key];
+                    $tempName = $_FILES['imgs']['tmp_name'][$key];
                     $randomFileNm = getRandomFileNm($originFileNm);
-                    if(move_uploaded_file($tempName,$saveDirectory . "/" . $randomFileNm)){
-                        // move_uploaded_file($tempName, $saveDirectory . "/test." . $ext);
-                        $param=[
-                            "ifeed" => $ifeed,
-                            "img" => $randomFileNm
-                        ];
-                        $this->model-> insFeedImg($param);
+                    if(move_uploaded_file($tempName, $saveDirectory . "/" . $randomFileNm)) {
+                        //chmod($saveDirectory . "/test." . $ext, octdec("0666"));
+                        //chmod("C:/Apache24/PHPgram/static/img/profile/1/test." . $ext, 0755);
+                        $paramImg["img"] = $randomFileNm;
+                        $this->model->insFeedImg($paramImg);
                     }
-                }
 
+                }
                 return ["result" => 1];
+            
+            
+            case _GET:
+                $page = 1;
+                if(isset($_GET["page"])) {
+                    $page = intval($_GET["page"]);
+                }
+                $startIdx = ($page - 1) * _FEED_ITEM_CNT;
+                $param = [
+                    "startIdx" => $startIdx,
+                    "iuser" => getIuser()
+                ];                
+                $list = $this->model->selFeedList($param);
+                foreach($list as $item){
+                    $item->imgList=$this->model->selFeedImgList($item);
+                }
+                return $list;
         }
     }
 }
